ddf8db31314f9f078c9d48ac776b174069fee1bb
tkakisu <takahirokakisu@gmail.com>
2022-08-18 10:28:21 +0000

    lifting-state-up

--- src/index.tsx
+++ src/index.tsx
@@ -2,29 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component<
-  { value: number },
-  { value: string | null }
-> {
-  constructor(props: { value: number }) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
+type SquareValue = 'X' | 'O' | null;
 
+type SquareProps = {
+  value: SquareValue;
+  onClick: () => void;
+};
+
+class Square extends React.Component<SquareProps, { value: string | null }> {
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: 'X' })}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
-class Board extends React.Component {
+class Board extends React.Component<any, { squares: SquareValue[] }> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i: number) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i: number) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
